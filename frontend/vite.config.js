e1394d8b08ca832063a4ed180d6fe1e85784ec6c
Yujie Ren <1047872312ren@gmail.com>
2026-03-12 16:26:19 +0800

    update API route

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://job-tracker.onrender.com/jobs',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
